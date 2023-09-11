13ba2924359a9b794dd337b856d5f879d18212bf
FIRST_NAME LAST_NAME <luijika711@gmail.com>
2023-09-11 11:20:48 +0300

    added C# solution and project

---
+++ server/ServerClient/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ServerClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[SERVER STARTED]");
+        }
+    }
+}
