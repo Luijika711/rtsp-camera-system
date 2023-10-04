6e9ca8b40a1f6776098e42301138155ed1b62355
luijika711 <luijika711@gmail.com>
2023-10-04 19:19:19 +0300

    made winforms project and managed to get live video from rtsp stream

---
+++ server/cameraViewer/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+using Accord;
+using Accord.Video;
+using Accord.Video.FFMPEG;
+using System.Drawing;
+using System.Threading;
+namespace cameraViewer
+{
+    public partial class Form1 : Form
+    {
+        VideoFileReader reader = new VideoFileReader();
+        void set_frame()
+        {
+            while (true)
+            {
+                Bitmap bmp = reader.ReadVideoFrame();
+                pictureBox1.Image = bmp;
+            }
+        }
+        public Form1()
+        {
+            reader.Open("rtsp://192.168.100.95:8080/h264_ulaw.sdp");
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ThreadStart childref = new ThreadStart(set_frame);
+            Thread childThread = new Thread(childref);
+            childThread.IsBackground = true;
+            childThread.Start();
+        }
+    }
+}
