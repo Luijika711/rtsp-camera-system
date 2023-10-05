fafb312a9dfd243225939fc4b70d1422f9eb2361
luijika711 <luijika711@gmail.com>
2023-10-05 14:59:22 +0300

    working on appending video,also might change libraries and use openCvsharp

--- server/cameraViewer/Form1.cs
+++ server/cameraViewer/Form1.cs
@@ -5,15 +5,43 @@ using Accord.Video;
 using Accord.Video.FFMPEG;
 using System.Drawing;
 using System.Threading;
+using System.Collections.Generic;
+
 namespace cameraViewer
 {
     public partial class Form1 : Form
     {
         public class VideoStreamWindow
         {
-            
-            public VideoFileReader reader = new VideoFileReader();
-            public VideoFileWriter videoWriter = new VideoFileWriter();
+            private VideoFileReader reader = new VideoFileReader();
+            private VideoFileWriter videoWriter = new VideoFileWriter();
+
+            public VideoStreamWindow(string ip,int idx)
+            {
+                //reader.Open(ip);
+                //Bitmap bmp = reader.ReadVideoFrame();
+                //reader.close();
+                reader.Open("camera_0.mp4");
+                long frameCount = reader.FrameCount;
+                List<Bitmap> bitmapList = new List<Bitmap>();
+                while (frameCount > 0)
+                {
+                    var img = reader.ReadVideoFrame();
+                    bitmapList.Add(img);
+                    frameCount--;
+                }
+                reader.Close();
+                reader.Open(ip);
+
+                videoWriter.Open("camera_" + idx.ToString() + ".mp4", 1920,1080, 30, VideoCodec.MPEG4, 25000);
+
+                foreach (Bitmap f in bitmapList)
+                {
+                    videoWriter.WriteVideoFrame(f);
+                }
+                
+                
+            }
             public void set_frame(ref PictureBox pictureBox1)
             {
                 while (true)
@@ -29,17 +57,15 @@ namespace cameraViewer
             }
         }
 
-        VideoStreamWindow vsw = new VideoStreamWindow();
+        VideoStreamWindow vsw = new VideoStreamWindow("rtsp://192.168.100.95:8080/h264_ulaw.sdp",0);
         public Form1()
         {
-            
-            vsw.reader.Open("rtsp://192.168.100.95:8080/h264_ulaw.sdp");
-            vsw.videoWriter.Open("thing.avi", 1920, 1080,30);
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
             Thread videoStreamThread = new Thread(() => vsw.set_frame(ref pictureBox1));
             videoStreamThread.IsBackground = true;
             videoStreamThread.Start();
