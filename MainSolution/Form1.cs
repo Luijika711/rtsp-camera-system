b7765241427d5e4cab3549a9005b6db9405c7dd3
luijika711 <luijika711@gmail.com>
2023-11-02 22:32:44 +0200

    added removing/adding camera functionality

---
+++ MainSolution/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Windows.Forms;
+using Accord;
+using Accord.Video;
+using Accord.Video.FFMPEG;
+using System.Drawing;
+using System.Threading;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using Accord.Vision.Detection.Cascades;
+using Accord.Vision.Detection;
+namespace cameraViewer
+{
+    public partial class Form1 : Form
+    {
+        public List<VideoStreamWindow> VideoStreams = new List<VideoStreamWindow>();
+        public List<Thread> VideoStreamThreads = new List<Thread>();
+        int VideoStreamIndex = -1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadCameras()
+        {
+            VideoStreams.Clear();
+            VideoStreamThreads.Clear();
+            int LineIndex = 0;
+            foreach (var line in File.ReadAllLines("cameras.txt"))
+            {
+                var CurrentVideoStreamWindow = new VideoStreamWindow(line.ToString(), LineIndex);
+                VideoStreams.Add(CurrentVideoStreamWindow);
+                LineIndex++;
+            }
+
+
+            if (VideoStreams.Count > 0)
+            {
+                VideoStreamIndex = 0;
+                foreach (var VideoStream in VideoStreams)
+                {
+                    Thread VideoStreamThread = new Thread(() => VideoStream.set_frame(ref pictureBox1));
+                    VideoStreamThread.IsBackground = true;
+                    VideoStreamThread.Start();
+                    VideoStreamThreads.Add(VideoStreamThread);
+                }
+
+                VideoStreams[VideoStreamIndex].IsActive = true;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VideoStreamIndex = 0;
+            LoadCameras();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            VideoStreamIndex--;
+            if(VideoStreamIndex < 0)
+            {
+                VideoStreamIndex = VideoStreams.Count - 1;
+            }
+            if (VideoStreamIndex > 0)
+            {
+                foreach (var v in VideoStreams)
+                    v.IsActive = false;
+                VideoStreams[VideoStreamIndex].IsActive = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (VideoStreams.Count > 0)
+            {
+                VideoStreamIndex++;
+                VideoStreamIndex %= (VideoStreams.Count);
+                foreach (var v in VideoStreams)
+                    v.IsActive = false;
+                VideoStreams[VideoStreamIndex].IsActive = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            File.AppendAllText("cameras.txt", "\n" + textBox1.Text);
+            foreach (var v in VideoStreams)
+                v.IsActive = false;
+            LoadCameras();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(VideoStreams.Count > 0)
+            {
+                String IpToRemove = VideoStreams[VideoStreamIndex].VideoIp;
+                var AllCameras = File.ReadAllLines("cameras.txt");
+                File.WriteAllText("cameras.txt", string.Empty);
+                foreach(var v in AllCameras)
+                {
+                    if(IpToRemove != v)
+                    {
+                        File.WriteAllText("cameras.txt", v + "\n");
+                    }
+                }
+                LoadCameras();
+            }
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"videos");
+        }
+    }
+}
---
+++ MainSolution/VideoStreamWindow.cs
@@ -0,0 +1,88 @@
+﻿using Accord.Video.FFMPEG;
+using Accord.Vision.Detection.Cascades;
+using Accord.Vision.Detection;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace cameraViewer
+{
+    public class VideoStreamWindow
+    {
+        public VideoFileReader Reader = new VideoFileReader();
+        public VideoFileWriter VideoWriter = new VideoFileWriter();
+        public bool IsActive = false;
+        string FileName;
+        private HaarObjectDetector faceDetector = new HaarObjectDetector(new FaceHaarCascade());
+        private DateTime lastNotificationTime = new DateTime();
+        int CameraNumber;
+        public string VideoIp;
+        public VideoStreamWindow(string ip, int idx)
+        {
+            Reader.Open(ip);
+            VideoIp = ip;
+            CameraNumber = idx;
+            Bitmap bmp = Reader.ReadVideoFrame();
+            FileName = "camera_" + idx.ToString();
+            Directory.CreateDirectory("videos/" + FileName);
+            int NumberOfVideos = System.IO.Directory.GetFiles("videos/" + FileName).Length;
+
+            //System.Diagnostics.Debug.Print(numberOfVideos.ToString());
+
+            VideoWriter.Open("videos/camera_" + idx.ToString() + "/" + NumberOfVideos.ToString() + ".avi", bmp.Width, bmp.Height, 30, VideoCodec.Default, 25000);
+        }
+        public void closeStreams()
+        {
+            VideoWriter.Close();
+            Reader.Close();
+        }
+        public void set_frame(ref PictureBox pictureBox1)
+        {
+            int frameIndex = 0;
+            while (true)
+            {
+                try
+                {
+
+                    Bitmap bmp = Reader.ReadVideoFrame();
+
+                    bool frameHasFace = false;
+                    if (frameIndex % 120 == 0)
+                    {
+                        var faceRectangles = faceDetector.ProcessFrame(bmp);
+                        if (faceRectangles.Length > 0)
+                        {
+                            frameHasFace = true;
+                        }
+                    }
+
+                    using (Graphics g = Graphics.FromImage(bmp))
+                    {
+                        g.FillRectangle(new SolidBrush(color: Color.White), new Rectangle(0, 0, 400, 30));
+                        g.DrawString(DateTime.Now.ToString() + ", camera " + CameraNumber.ToString(), new Font("Arial", 20), new SolidBrush(color: Color.Black), 0, 0);
+                    }
+
+                    if (frameHasFace == true)//code goes here
+                    {
+                        Debug.Print("[FACE FOUND]");
+                    }
+
+                    VideoWriter.WriteVideoFrame(bmp);
+                    if (IsActive == true)
+                        pictureBox1.Image = bmp;
+                    frameIndex++;
+                }
+                catch (Exception e)
+                {
+                    Debug.Print(e.Message);
+                    continue;
+                }
+            }
+        }
+    }
+}
